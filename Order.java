ece0e9b9d722109d08b9f1f213cdc7fe882ec1ee
EstebanPdN <72305261+EstebanPdN@users.noreply.github.com>
2026-04-22 15:33:14 +0000

    Added methods for removing items and calculating total

--- Order.java
+++ Order.java
@@ -8,12 +8,39 @@ public class Order {
         items = new ArrayList<>();
     }
 
-    // Add item to order
+    // Add item
     public void addItem(Item item) {
         items.add(item);
     }
 
-    // Get all items
+    // Remove item
+    public void removeItem(String name) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equalsIgnoreCase(name)) {
+                items.remove(i);
+                break;
+            }
+        }
+    }
+
+    // Calculate total
+    public double calculateTotal() {
+        double total = 0;
+        for (Item item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    // Display order
+    public void displayOrder() {
+        for (Item item : items) {
+            System.out.println(item);
+        }
+        System.out.println("Total: $" + calculateTotal());
+    }
+
+    // Getter
     public ArrayList<Item> getItems() {
         return items;
     }
